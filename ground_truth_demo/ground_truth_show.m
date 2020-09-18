f6277ec51f8b6bbf00c225628b7997e11779d678
LIM HYUN SEOK <cr3ux53c@gmail.com>
2020-09-18 20:46:01 +0900

    Support for Matlab 2020a

--- ground_truth_demo/ground_truth_show.m
+++ ground_truth_demo/ground_truth_show.m
@@ -6,18 +6,20 @@
 %   
 %   The code and the algorithm are for non-comercial use only.
 
-videoIdx = 1; 
 
-obj = mmreader(['testing_videos\',sprintf('%.2d',videoIdx),'.avi']);
-numFrames = get(obj, 'NumberOfFrames');
-load(['testing_label_mask\',num2str(videoIdx),'_label']);
+for i = 1 : 21
+    videoIdx = i;
+    obj = VideoReader(['testing_videos\',sprintf('%.2d',videoIdx),'.avi']);
+    numFrames = get(obj, 'NumFrames');
+    load(['testing_label_mask\',num2str(videoIdx),'_label']);
 
- 
 
-for ii = 1 : numFrames
-    mask = volLabel{ii};
-    curFrame = im2double(read(obj, ii));
-    curFrame(:,:,1) = min(curFrame(:,:,1) +  0.5*double(mask), 1); 
-    imshow(curFrame);
-    getframe;
+    for ii = 1 : numFrames
+        disp([sprintf('%02d Video''s frame #', i), sprintf('%04d', ii)])
+        mask = volLabel{ii};
+        curFrame = im2double(read(obj, ii));
+        curFrame(:,:,1) = min(curFrame(:,:,1) +  0.5*double(mask), 1); 
+        imshow(curFrame);
+        f = getframe;
+    end
 end
